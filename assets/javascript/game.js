e314115e0aa064b6ee8dfc15416341dea8fc457d
Natalie Buford <nataliebuford@gmail.com>
2020-05-10 16:22:21 -0500

    with lots of help

---
+++ assets/javascript/game.js
@@ -0,0 +1,55 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var letterToGuess = null;
+var guessedLetters = [];
+var guessesLeft = 7;
+
+var updateGuessesLeft = function() {
+  document.querySelector("#guesses-left").innerHTML = guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+};
+
+var updateGuessesSoFar = function() {
+  document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+};
+
+
+var reset = function() {
+  guessesLeft = 7;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+};
+
+
+  updateLetterToGuess();
+  updateGuessesLeft();
+
+
+document.onkeydown = function(event) {
+  guessesLeft--;
+  var letter = event.key.toLowerCase();
+  guessedLetters.push(letter);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+
+
+  if (letter === letterToGuess) {
+    wins++;
+    document.querySelector("#wins").innerHTML = wins;
+    reset();
+  }
+
+
+  if (guessesLeft === 0) {
+    losses++;
+    document.querySelector("#losses").innerHTML = losses;
+    reset();
+  }
+};
